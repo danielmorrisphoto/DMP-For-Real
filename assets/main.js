6001413e0ef7c350976ad1fa103a4289154ade0a
Daniel Morris <daniel@danielmorrisphoto.com>
2017-06-22 19:20:43 -0400

    added landing images, logo images,

---
+++ assets/main.js
@@ -0,0 +1,23 @@
+$(function(){
+    $('.carousel').carousel({
+      interval: 5000
+    });
+});
+
+var checkitem = function() {
+    var $this;
+    $this = $("#slideshow");
+        if ($("#slideshow .carousel-inner .item:first").hasClass("active")) {
+        $this.children(".left").hide();
+        $this.children(".right").show();
+} else if ($("#slideshow .carousel-inner .item:last").hasClass("active")) {
+        $this.children(".right").hide();
+        $this.children(".left").show();
+} else {
+        $this.children(".carousel-control").show();
+}
+};
+
+checkitem();
+
+$("#slideshow").on("slid.bs.carousel", "", checkitem);
