c83e4053350678ea373e569afe0b372eba02f5a4
Daniel Morris <daniel@danielmorrisphoto.com>
2017-06-25 19:09:27 -0400

    index: image carousel is responsive but the rest of the page is not portafolio: just started

--- assets/main.js
+++ assets/main.js
@@ -1,23 +1,26 @@
-$(function(){
-    $('.carousel').carousel({
-      interval: 5000
-    });
+
+$(document).ready(function() {
+        $('.carousel').carousel({
+                interval: 5000
+        })
 });
 
-var checkitem = function() {
-    var $this;
-    $this = $("#slideshow");
-        if ($("#slideshow .carousel-inner .item:first").hasClass("active")) {
-        $this.children(".left").hide();
-        $this.children(".right").show();
-} else if ($("#slideshow .carousel-inner .item:last").hasClass("active")) {
-        $this.children(".right").hide();
-        $this.children(".left").show();
-} else {
-        $this.children(".carousel-control").show();
-}
-};
 
-checkitem();
+// WHY DID I ADD THIS FUCNTION FOR?
+// var checkitem = function() {
+//     var $this;
+//     $this = $("#slideshow");
+//         if ($("#slideshow .carousel-inner .item:first").hasClass("active")) {
+//         $this.children(".left").hide();
+//         $this.children(".right").show();
+// } else if ($("#slideshow .carousel-inner .item:last").hasClass("active")) {
+//         $this.children(".right").hide();
+//         $this.children(".left").show();
+// } else {
+//         $this.children(".carousel-control").show();
+// }
+// };
 
-$("#slideshow").on("slid.bs.carousel", "", checkitem);
+// checkitem();
+
+// $("#slideshow").on("slid.bs.carousel", "", checkitem);
