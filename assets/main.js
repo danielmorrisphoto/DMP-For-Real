7fbc8c31944d78e45180751995c9bb8dcce1ed57
Daniel Morris <daniel@danielmorrisphoto.com>
2017-06-26 23:09:37 -0400

    RI is amazing!!! carousel zoom had been implemented but needs tweaking

--- assets/main.js
+++ assets/main.js
@@ -6,21 +6,4 @@ $(document).ready(function() {
 });
 $("div.navbar-fixed-top").autoHidingNavbar();
 
-// WHY DID I ADD THIS FUCNTION FOR?
-// var checkitem = function() {
-//     var $this;
-//     $this = $("#slideshow");
-//         if ($("#slideshow .carousel-inner .item:first").hasClass("active")) {
-//         $this.children(".left").hide();
-//         $this.children(".right").show();
-// } else if ($("#slideshow .carousel-inner .item:last").hasClass("active")) {
-//         $this.children(".right").hide();
-//         $this.children(".left").show();
-// } else {
-//         $this.children(".carousel-control").show();
-// }
-// };
 
-// checkitem();
-
-// $("#slideshow").on("slid.bs.carousel", "", checkitem);
