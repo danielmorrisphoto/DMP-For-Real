425d599aefe5853ae7801a91d5401eda9e8000cc
Daniel Morris <daniel@danielmorrisphoto.com>
2017-06-27 00:30:54 -0400

    HOLY SHIT!!! RI IS EVEN MORE AMAZING!!! zooming carousel fixed. show specific image over hover fixed **need to tweak zoom effect -  too drastic at the moment** **on hover zoom effect needs to stop**

--- assets/main.js
+++ assets/main.js
@@ -7,3 +7,41 @@ $(document).ready(function() {
 $("div.navbar-fixed-top").autoHidingNavbar();
 
 
+// Cuando el mouse esta sobre el texto con id portrait
+// ense~ana la foto con id portrait_foto
+
+
+$(document).ready(function () {
+    $('#portrait_label').hover(function(){
+        $('.carousel-inner').children().removeClass('active');
+        $('#portrait_foto').addClass('active'); //to make text underlined on hover
+        $('.carousel').carousel('pause');
+        /// $('#image').show(); //displays image on mouse in
+    },function(){
+        $('.carousel').carousel('cycle'); //remove underline on mouse out
+    });
+
+    $('#overview_label').hover(function(){
+        $('.carousel-inner').children().removeClass('active');
+        $('#overview_foto').addClass('active'); //to make text underlined on hover
+        $('.carousel').carousel('pause');
+        /// $('#image').show(); //displays image on mouse in
+    },function(){
+        $('.carousel').carousel('cycle'); //remove underline on mouse out
+    });
+
+    $('#travel_label').hover(function(){
+        $('.carousel-inner').children().removeClass('active');
+        $('#travel_foto').addClass('active'); //to make text underlined on hover
+        $('.carousel').carousel('pause');
+        /// $('#image').show(); //displays image on mouse in
+    },function(){
+        $('.carousel').carousel('cycle'); //remove underline on mouse out
+    });
+});
+
+// $('#formcontainer').hover(function(){
+//    $("#myCarousel4").carousel('pause');
+// },function(){
+//    $("#myCarousel4").carousel('cycle');
+// });
