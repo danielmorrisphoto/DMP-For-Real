1f2952257e8c78b728f0afeec6bc6e4ea0477962
Daniel Morris <daniel@danielmorrisphoto.com>
2017-11-17 17:32:05 -0400

    eliminated bad code

--- assets/index.js
+++ assets/index.js
@@ -26,27 +26,3 @@ $('.carousel').carousel({
 interval: 4000,
 pause: "false"
 });
-/*full image background*/
-/*
-var $item = $('.full-background'); 
-var $wHeight = $(window).height();
-$item.eq(0).addClass('active');
-$item.height($wHeight); 
-$item.addClass('full-screen');
-
-$('.full-background img').each(function() {
-var $src = $(this).attr('src');
-var $color = $(this).attr('data-color');
-$(this).parent().css({
-    'background-image' : 'url(' + $src + ')',
-    'background-color' : $color
-});
-$(this).remove();
-});
-
-$(window).on('resize', function (){
-$wHeight = $(window).height();
-$item.height($wHeight);
-});
-
-*/
